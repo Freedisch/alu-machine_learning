bd7070b39f0704eca0bffbfde8668d9f3ffa32f0
freedisch <freeproduc@gmail.com>
2024-02-02 22:56:57 +0200

    hot fix
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,59 +1,43 @@
 #!/usr/bin/env python3
-"""
-Defines function that updates the weights with Dropout regularization
-using gradient descent
-"""
+'''gradient_descent with L2 regularization'''
 
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """
-    Updates the weights with Dropout regularization using gradient descent
-
-    parameters:
-        Y [one-hot numpy.ndarray of shape (classes, m)]:
-            contains the correct labels for the data
-            classes: number of classes
-            m: number of data points
-        weights [dict]:
-            contains the weights and biases of the network
-        cache [dict]:
-            contains the outputs and dropout masks of each layer
-        alpha [float]:
-            learning rate
-        keep_prob [float]:
-            the probability that a node will be kept
-        L [int]:
-            number of layers in the network
-
-    all layers should use the tanh activation function except last
-    last layer should use softmax activation function
-
-    the weights of the network should be updated in place
-    """
-    m = Y.shape[1]
-    back = {}
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-            dz = back["dz{}".format(index)]
-
-        else:
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-            dz = back["dz{}".format(index)]
-            dz *= cache["D{}".format(index)]
-            dz /= keep_prob
-
-        dW = (1 / m) * (np.matmul(dz, A.transpose()))
+    '''updates the weights of a neural network with Dropout
+        regularization using gradient descen
+    Args:
+        @Y: is a one-hot numpy.ndarray of shape (classes, m) that contains
+            the correct labels for the data
+            classes: is the number of classes
+            m: is the number of data points
+        weights: dictionary of the weights and biases of the neural network
+        cache: dictionary of the outputs and dropout masks of each layer of
+                the neural network
+        alpha: is the learning rate
+        keep_prob is the probability that a node will be kept
+        L: is the number of layers of the network
+    Return
+        weights and biases of the network should be updated in place
+    '''
+    m = len(Y[1])
+    # derivative of last Z 'network output'
+    dz = cache['A'+str(L)] - Y  # loss, error
+    # from that point make the backpropagation
+    for i in range(L, 0, -1):
+        # derivative of bias bi
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        W_prev = weights["W{}".format(index)]
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+        # derivative of weight wi
+        dW = ((1 / m) * np.matmul(dz, cache['A'+str(i-1)].T))
+        # derivative of Zi with dropout
+        if i - 1 > 0:
+            dz = np.matmul(weights['W'+str(i)].T, dz) *\
+                ((1 - cache['A'+str(i-1)] ** 2)) *\
+                (cache['D'+str(i-1)] / keep_prob)
+
+        # update weights and bias
+        weights['W'+str(i)] = weights['W'+str(i)] -\
+            (alpha * dW)
+        weights['b'+str(i)] = weights['b'+str(i)] -\
+            (alpha * db)
